38241be5e2a8d729a1879a3e2b8d16a0e1361c18
Weronika <kusidelw@gmail.com>
2026-03-24 12:53:13 +0100

    zmiana styli

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Strona Główna</title> 
+    <title>LEARN IT</title> 
     <link rel="icon" type="image/x-icon" href=x>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
@@ -12,7 +12,7 @@
 <body>
 
 <header>
-    <h1>Strona Główna</h1>
+    <h1>LEARN IT</h1>
     <nav class="nav">
         <ul class="nav-list">
             <li><a href="index.php">Strona Główna</a></li>
