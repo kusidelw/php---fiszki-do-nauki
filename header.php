5b3aaea8ae0774bd30ca9ee6aea0c6cc9dc04d69
Weronika <kusidelw@gmail.com>
2026-03-24 12:28:58 +0100

    zmiana głównego toku strony

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona Główna</title> 
+    <link rel="icon" type="image/x-icon" href=x>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    
+</head>
+<body>
+
+<header>
+    <h1>Strona Główna</h1>
+    <nav class="nav">
+        <ul class="nav-list">
+            <li><a href="index.php">Strona Główna</a></li>
+            <li><a href=# > Fiszki</a></li>
+            <li><a href=#> Zaloguj </a></li>
+        </ul>
+    </nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<main> 
+
+
+
+</main>
+
+<?php include 'footer.php'; ?>
